401fb29d1d70f93a722ae91f7ee84894cb17df11
MasudRana15924 <masudrana15924@outlook.com>
2023-07-08 22:03:58 +0600

    loader added

--- src/pages/loader/Loader.js
+++ src/pages/loader/Loader.js
@@ -4,17 +4,17 @@ import logo from '../../images/diu.png'
 const Loader = () => {
     return (
         <div className="container w-2/4 mx-auto md:w-2/4 lg:w-1/4 lg:mx-auto mt-96 lg:mt-96 justify-items-center">
-        <div className="ml-10 w-2/4 mx-auto lg:ml-44 lg:w-1/4 ">
-                {/* <Triangle
-                    height="60"
-                    width="60"
+        <div className="ml-10 w-2/4 mx-auto lg:ml-28 lg:w-1/4 ">
+                <Triangle
+                    height="70"
+                    width="70"
                     color="#F510EB "
                     ariaLabel="triangle-loading"
                     wrapperStyle={{}}
                     wrapperClassName=""
                     visible={true}
-                /> */}
-               <img src={logo} alt="" />
+                />
+               {/* <img src={logo} alt="" /> */}
                
         </div>
         {/* <p className="ml-3 lg:ml-30 text-sm font-semibold leading-6 text-gray-900 mt-3">MKM HelathBridge</p> */}
